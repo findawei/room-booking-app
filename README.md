2d918485fcddb5c044fb88ba7055bc7bdefbeb7a
Alex Benjamignan <hayhoky@gmail.com>
2022-06-29 08:50:36 -0400

    Completed

---
+++ README.md
@@ -0,0 +1,14 @@
+This is booking app
+
+The booking system has the following functionalities:
+• Users can see meeting rooms availability
+• Users can book meeting rooms by the hour (first come first served)
+• Users can cancel their own reservations
+
+## To Install
+
+npm i
+
+## To Run
+
+ionic serve
--- src/components/CompanyPicker.tsx
+++ src/components/CompanyPicker.tsx
@@ -5,13 +5,10 @@ import {
   IonCol,
   IonContent,
   IonButton,
-  IonHeader,
   IonDatetime,
   IonList,
-  IonItemDivider,
   IonItem,
   IonLabel,
-  IonCheckbox,
   IonRadio,
   IonListHeader,
   IonRadioGroup,
@@ -19,17 +16,23 @@ import {
   IonCardContent,
   IonIcon,
   IonCardHeader,
-  IonCardSubtitle,
   IonCardTitle,
+  IonText,
 } from "@ionic/react";
+import {
+  businessOutline,
+  calendarOutline,
+  checkboxOutline,
+} from "ionicons/icons";
 import { meetingRooms, sampleData } from "../data/SampleData";
+import image from "../data/card-top-img.png";
 
 interface ContainerProps {}
 
 const CompanyPicker: React.FC<ContainerProps> = () => {
   const [company, setCompany] = useState("");
   const [startTime, setStartTime] = useState("");
-  const [selected, setSelected] = useState("");
+  const [room, setRoom] = useState("");
   const [confirmTime, setConfirmTime] = useState(false);
 
   const filteredRooms = meetingRooms.map((room) => ({
@@ -37,54 +40,92 @@ const CompanyPicker: React.FC<ContainerProps> = () => {
     isBooked: sampleData.filter((booking) => booking.room === room).length
       ? true
       : false,
-    // company: sampleData.filter((booking) => booking.room === room).company,
   }));
 
-  console.log({ company, startTime, selected });
+  // console.log({ company, startTime, selected });
+
+  const resetData = () => {
+    setCompany("");
+    setStartTime("");
+    setRoom("");
+    setConfirmTime(false);
+  };
 
   return (
     <IonContent>
-      <IonGrid>
+      <IonGrid style={{ height: "100%" }}>
         {!company ? (
-          <IonRow className="ion-align-items-center">
-            Pick a company
-            <IonCol>
-              <IonButton onClick={() => setCompany("COKE")}>Coke</IonButton>
-            </IonCol>
-            <IonCol>
-              <IonButton onClick={() => setCompany("PEPSI")}>Pepsi</IonButton>
-            </IonCol>
-          </IonRow>
+          <>
+            <IonRow class="ion-align-items-center" style={{ height: "50%" }}>
+              <IonCol class="ion-text-center">
+                <IonText>
+                  <h1>Pick Your Company</h1>
+                </IonText>
+              </IonCol>
+            </IonRow>
+            <IonRow class="ion-align-items-center">
+              <IonCol class="ion-text-center">
+                <IonButton
+                  size="large"
+                  color="danger"
+                  onClick={() => setCompany("Coke")}
+                >
+                  Coke
+                </IonButton>
+              </IonCol>
+              <IonCol class="ion-text-center">
+                <IonButton size="large" onClick={() => setCompany("Pepsi")}>
+                  Pepsi
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </>
         ) : confirmTime == false ? (
-          <IonRow className="ion-align-items-center">
-            <IonCol>
-              Pick a start time
-              <IonDatetime
-                presentation="time"
-                minuteValues="0"
-                hourValues={[7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6]}
-                onIonChange={(e: any) => setStartTime(e.detail.value)}
-              ></IonDatetime>
-            </IonCol>
-            <IonCol>
-              <IonButton onClick={() => setConfirmTime(true)}>
-                Confirm
-              </IonButton>
-            </IonCol>
-          </IonRow>
-        ) : selected == "" ? (
+          <>
+            <IonRow class="ion-align-items-center" style={{ height: "10%" }}>
+              <IonCol class="ion-text-center">
+                <IonText>
+                  <h1>Pick a start time</h1>
+                </IonText>
+              </IonCol>
+            </IonRow>
+            <IonRow
+              className="ion-align-items-center"
+              // style={{ height: "30%" }}
+            >
+              <IonCol class="ion-text-center">
+                <IonDatetime
+                  size="cover"
+                  presentation="time"
+                  minuteValues="0"
+                  hourValues={[7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6]}
+                  onIonChange={(e: any) => setStartTime(e.detail.value)}
+                ></IonDatetime>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol class="ion-text-center">
+                <IonButton onClick={() => setConfirmTime(true)}>
+                  Confirm
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </>
+        ) : room == "" ? (
           <IonList>
             <IonRadioGroup
-              value={selected}
-              onIonChange={(e) => setSelected(e.detail.value)}
+              value={room}
+              onIonChange={(e) => setRoom(e.detail.value)}
             >
               <IonListHeader>
                 <IonLabel>
-                  Pick a room for your meeting at {""}
-                  {new Date(startTime).toLocaleTimeString("en", {
-                    timeStyle: "short",
-                    hour12: true,
-                  })}
+                  <h1>
+                    Pick a room for your meeting at {""}
+                    {new Date(startTime).toLocaleTimeString("en", {
+                      timeStyle: "short",
+                      hour12: true,
+                    })}
+                  </h1>
                 </IonLabel>
               </IonListHeader>
               {filteredRooms.map(({ room, isBooked }, i) => (
@@ -103,16 +144,57 @@ const CompanyPicker: React.FC<ContainerProps> = () => {
           </IonList>
         ) : (
           <IonCard>
+            <img src={image} width="100%" />
             <IonCardHeader>
-              <IonCardTitle>
-                You booked room {selected} from {startTime} for 1 hour
-              </IonCardTitle>
+              <IonCardTitle>You're booked!</IonCardTitle>
             </IonCardHeader>
 
             <IonCardContent>
-              Keep close to Nature's heart... and break clear away, once in
-              awhile, and climb a mountain or spend a week in the woods. Wash
-              your spirit clean.
+              <IonList>
+                <IonItem>
+                  <IonIcon slot="start" icon={businessOutline}></IonIcon>
+                  <IonLabel>
+                    <h2>{company}</h2>
+                    <p>Company</p>
+                  </IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonIcon slot="start" icon={checkboxOutline}></IonIcon>
+                  <IonLabel>
+                    <h2>{room}</h2>
+                    <p>Room</p>
+                  </IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonIcon slot="start" icon={calendarOutline}></IonIcon>
+                  <IonLabel>
+                    <h2>
+                      {" "}
+                      {new Date(startTime).toLocaleTimeString("en", {
+                        timeStyle: "short",
+                        hour12: true,
+                      })}{" "}
+                      to{" "}
+                      {new Date(
+                        new Date(startTime).getTime() + 1 * 60 * 60 * 1000
+                      ).toLocaleTimeString("en", {
+                        timeStyle: "short",
+                        hour12: true,
+                      })}
+                    </h2>
+                    <p>Time</p>
+                  </IonLabel>
+                </IonItem>
+              </IonList>
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonButton size="small" onClick={() => resetData()}>
+                      Cancel
+                    </IonButton>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
             </IonCardContent>
           </IonCard>
         )}
